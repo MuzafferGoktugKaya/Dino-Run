b0fe03c578562b2362aaa9e9f20c1351e2b75e70
Ras T <erenceliktener@gmail.com>
2026-05-22 08:39:24 +0300

    feat: apply weather effect to air obstacle speed

--- Assets/AirZone/Scripts/Obstacles/AirObstacle.cs
+++ Assets/AirZone/Scripts/Obstacles/AirObstacle.cs
@@ -1,17 +1,53 @@
 using UnityEngine;
+using AirZone.Weather;
 
+[DisallowMultipleComponent]
 public class AirObstacle : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private float moveSpeed = 8f;
     [SerializeField] private float destroyZ = -15f;
 
+    [Header("Weather Speed Effect")]
+    [SerializeField] private float forwardWindSpeedBonus = 4f;
+    [SerializeField] private float backwardWindSpeedPenalty = 4f;
+    [SerializeField] private float minimumMoveSpeed = 1f;
+
+    private AirWeatherManager weatherManager;
+
+    private void Awake()
+    {
+        weatherManager = FindFirstObjectByType<AirWeatherManager>();
+    }
+
     private void Update()
     {
-        transform.Translate(Vector3.back * moveSpeed * Time.deltaTime, Space.World);
+        float finalMoveSpeed = GetWeatherAdjustedSpeed();
+
+        transform.Translate(Vector3.back * finalMoveSpeed * Time.deltaTime, Space.World);
 
         if (transform.position.z <= destroyZ)
         {
             Destroy(gameObject);
         }
+    }
+
+    private float GetWeatherAdjustedSpeed()
+    {
+        float finalMoveSpeed = moveSpeed;
+
+        if (weatherManager == null || weatherManager.CurrentEffect == null)
+            return finalMoveSpeed;
+
+        if (weatherManager.CurrentEffect.Type == AirWeatherType.ForwardWind)
+        {
+            finalMoveSpeed += forwardWindSpeedBonus;
+        }
+        else if (weatherManager.CurrentEffect.Type == AirWeatherType.BackwardWind)
+        {
+            finalMoveSpeed -= backwardWindSpeedPenalty;
+        }
+
+        return Mathf.Max(minimumMoveSpeed, finalMoveSpeed);
     }
 }

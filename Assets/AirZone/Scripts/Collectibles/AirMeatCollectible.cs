a9265eef4f3f6de84bd9985fd37699fa3a1383f0
Ras T <erenceliktener@gmail.com>
2026-05-23 21:32:29 +0300

    feat: add air meat collectible

---
+++ Assets/AirZone/Scripts/Collectibles/AirMeatCollectible.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class AirMeatCollectible : MonoBehaviour
+{
+    [Header("Collectible")]
+    [SerializeField] private int meatAmount = 1;
+
+    private bool isCollected;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isCollected)
+            return;
+
+        IAirMeatConsumer meatConsumer = other.GetComponentInParent<IAirMeatConsumer>();
+
+        if (meatConsumer == null)
+            return;
+
+        isCollected = true;
+        meatConsumer.AddMeat(meatAmount);
+
+        Debug.Log($"[AirMeat] Collected meat: {meatAmount}");
+
+        Destroy(gameObject);
+    }
+}

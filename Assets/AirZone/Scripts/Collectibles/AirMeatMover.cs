7de636a46ce07baa1bd92c89e948f017436004ca
Ras T <erenceliktener@gmail.com>
2026-05-31 16:55:53 +0300

    fix: tune air spawn spacing

--- Assets/AirZone/Scripts/Collectibles/AirMeatMover.cs
+++ Assets/AirZone/Scripts/Collectibles/AirMeatMover.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using AirZone.Weather;
 
 [DisallowMultipleComponent]
 public class AirMeatMover : MonoBehaviour
@@ -7,14 +8,47 @@ public class AirMeatMover : MonoBehaviour
     [SerializeField] private float moveSpeed = 8f;
     [SerializeField] private float returnZ = -10f;
 
+    [Header("Weather Speed Effect")]
+    [SerializeField] private float forwardWindSpeedBonus = 4f;
+    [SerializeField] private float backwardWindSpeedPenalty = 4f;
+    [SerializeField] private float minimumMoveSpeed = 1f;
+
+    private AirWeatherManager weatherManager;
+
+    private void Awake()
+    {
+        weatherManager = FindFirstObjectByType<AirWeatherManager>();
+    }
+
     private void Update()
     {
-        transform.Translate(Vector3.back * moveSpeed * Time.deltaTime, Space.World);
+        float finalMoveSpeed = GetWeatherAdjustedSpeed();
+
+        transform.Translate(Vector3.back * finalMoveSpeed * Time.deltaTime, Space.World);
 
         if (transform.position.z <= returnZ)
         {
             ReturnToPoolOrDestroy();
         }
+    }
+
+    private float GetWeatherAdjustedSpeed()
+    {
+        float finalMoveSpeed = moveSpeed;
+
+        if (weatherManager != null && weatherManager.CurrentEffect != null)
+        {
+            if (weatherManager.CurrentEffect.Type == AirWeatherType.ForwardWind)
+            {
+                finalMoveSpeed += forwardWindSpeedBonus;
+            }
+            else if (weatherManager.CurrentEffect.Type == AirWeatherType.BackwardWind)
+            {
+                finalMoveSpeed -= backwardWindSpeedPenalty;
+            }
+        }
+
+        return Mathf.Max(minimumMoveSpeed, finalMoveSpeed);
     }
 
     private void ReturnToPoolOrDestroy()
